fcaa5c09cd6e75f937b74ee9b8268bc91fd28b8d
Brandon Abutin <babut001@ucr.edu>
2020-03-16 21:26:11 -0700

    Make stack location random and add aslr test

--- exec.c
+++ exec.c
@@ -7,6 +7,31 @@
 #include "x86.h"
 #include "elf.h"
 
+uint random(void) {
+  static unsigned int z1 = 12345, z2 = 12345, z3 = 12345, z4 = 12345;
+  unsigned int b;
+  b  = ((z1 << 6) ^ z1) >> 13;
+  z1 = ((z1 & 4294967294U) << 18) ^ b;
+  b  = ((z2 << 2) ^ z2) >> 27;
+  z2 = ((z2 & 4294967288U) << 2) ^ b;
+  b  = ((z3 << 13) ^ z3) >> 21;
+  z3 = ((z3 & 4294967280U) << 7) ^ b;
+  b  = ((z4 << 3) ^ z4) >> 12;
+  z4 = ((z4 & 4294967168U) << 13) ^ b;
+
+  return (z1 ^ z2 ^ z3 ^ z4) / 2;
+}
+
+int randomrange(int lo, int hi) {
+  if (hi < lo) {
+    int tmp = lo;
+    lo = hi;
+    hi = tmp;
+  }
+  int range = hi - lo + 1;
+  return random() % (range) + lo;
+}
+
 int
 exec(char *path, char **argv)
 {
@@ -63,7 +88,7 @@ exec(char *path, char **argv)
 
   // Allocate two pages at the next page boundary.
   // Make the first inaccessible.  Use the second as the user stack.
-  sp = PGROUNDUP(sz) + 50*PGSIZE;
+  sp = PGROUNDUP(sz) + randomrange(50,100)*PGSIZE;
   if((sp = allocuvm(pgdir, sp, sp + 2*PGSIZE)) == 0)
     goto bad;
   clearpteu(pgdir, (char*)(sp - 2*PGSIZE));
