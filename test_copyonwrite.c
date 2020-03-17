db6f8fd476ec25fc1390f5d0a49c6ce6d0dd5138
Brandon Abutin <babut001@ucr.edu>
2020-03-17 12:57:15 -0700

    Track number of free pages and add test_copyonwrite

---
+++ test_copyonwrite.c
@@ -0,0 +1,40 @@
+#include "types.h"
+#include "user.h"
+#include "stat.h"
+
+int main(void) {
+  int data = 1;
+  int pid;
+
+  printf(1, "Number of initial free pages: %d\n", getfreepages());
+
+  pid = fork();
+  if(pid == 0) {
+    printf(1, "Number of free pages in child before changing data: %d\n", getfreepages());
+    data = 2;
+    printf(1, "Number of free pages in child after changing data: %d\n", getfreepages());
+    pid = fork();
+    if(pid == 0) {
+      printf(1, "Number of free pages in nested child before changing data: %d\n", getfreepages());
+      data = 3;
+      printf(1, "Number of free pages in nested child after changing data: %d\n", getfreepages());
+    } else {
+      wait();
+      printf(1, "Number of free pages in child after wait: %d\n", getfreepages());
+    }
+  } else {
+    wait();
+    printf(1, "Number of free pages in parent after wait: %d\n", getfreepages());
+    pid = fork();
+    if(pid == 0) {
+      printf(1, "Number of free pages in child2 before changing data: %d\n", getfreepages());
+      data = 4;
+      printf(1, "Number of free pages in child2 after changing data: %d\n", getfreepages());
+    } else {
+      wait();
+      printf(1, "Number of free pages in parent after wait: %d\n", getfreepages());
+    }
+  }
+
+  exit();
+}
